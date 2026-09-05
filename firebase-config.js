f52aa6bf5a9946844d0ea6f13e939897415df786
chyrxxx-bit <chyrxxx@gmail.com>
2026-09-06 07:01:03 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase Web App configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFkcqCLhW9FZRfRyL7HMfuFwJts7kDslg",
+  authDomain: "yulim-54c48.firebaseapp.com",
+  projectId: "yulim-54c48",
+  storageBucket: "yulim-54c48.firebasestorage.app",
+  messagingSenderId: "7924109236",
+  appId: "1:7924109236:web:9ecb69dad5f4097cca2ce1"
+};
